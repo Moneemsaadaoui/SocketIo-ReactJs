611e57bca2ac1cd5db00d2c7fd5e5253f0b18bd2
unknown <abdelmoneem.saadaoui@satoripop.com>
2018-08-29 09:12:07 +0100

    Running locally

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.set('origins', '*:*');
+io.on('connection', async (socket: any) => {
+	console.log("Client Successfully Connected");
+
+	io.emit('chat', "hello world");
+})
+
+server.listen(5000, () => {
+	console.log("Backend Server is running on http://localhost:5000");
+})
---
+++ Frontend/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Chat from 'components/Chat.js'
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <Chat />
+    )
+  }
+}
+
+AppComponent.defaultProps = {
+}
+
+export default AppComponent
